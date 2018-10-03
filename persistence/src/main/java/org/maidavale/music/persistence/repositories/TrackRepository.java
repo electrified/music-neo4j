0fb95d827eb3151402703833663991c1b5f9738d
ed <ebwibble@hotmail.com>
2018-10-04 00:03:21 +0100

    enable rest repositories, remove unneeded static files and controllers

--- persistence/src/main/java/org/maidavale/music/persistence/repositories/TrackRepository.java
+++ persistence/src/main/java/org/maidavale/music/persistence/repositories/TrackRepository.java
@@ -1,12 +1,14 @@
 package org.maidavale.music.persistence.repositories;
 
 import org.maidavale.music.persistence.domain.Track;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 
 public interface TrackRepository extends Neo4jRepository<Track, Long> {
+    @Query("MATCH (n:`Track`) WHERE LOWER(n.title) =~ LOWER('.*' + { `title` } + '.*') WITH n RETURN n,[ [ (n)-[r_b1:`BY`]->(a1:`Artist`) | [ r_b1, a1 ] ], [ (n)-[r_a1:`APPEARS_ON`]->(r1:`Release`) | [ r_a1, r1 ] ], [ (n)-[r_f1:`FILE_TO_TRACK`]->(a1:`AudioFile`) | [ r_f1, a1 ] ] ]")
     Collection<Track> findByTitleLike(@Param("title") final String title);
     Collection<Track> findTrackByArtistContainingOrTitleContainingOrGenreContaining(@Param("artist") final String artist,
                                                                                     @Param("title") final String title,
